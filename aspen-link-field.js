6a61afcb7d2fd2c6489c89deca6b17414dcbff36
ishansubedi <ishan@appmozart.com>
2020-03-10 17:27:44 -0700

    corrected the linkFldMixinFile

--- aspen-link-field.js
+++ aspen-link-field.js
@@ -1,7 +1,7 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
-import { AspenLinkFieldMixin } from './aspen-link-field-mixin.js'
-import './aspen-field-wrapper.js'
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
+import { AspenLinkFldMixin } from "./aspen-link-field-mixin.js";
+import "./aspen-field-wrapper.js";
 
 /**
  * `asp-link-fld` Description
@@ -11,51 +11,47 @@ import './aspen-field-wrapper.js'
  * @polymer
  * @extends {Polymer.Element}
  */
-class AspenLinkField extends AspenLinkFieldMixin(PolymerElement) {
+class AspenLinkField extends AspenLinkFldMixin(PolymerElement) {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
 
-    static get template() {
-        return html`
-        <style>
-			:host {
-				display: block;
-			    }
-        </style>
-        
-		<aspen-field-wrapper label = [[label]] icon ="[[icon]]" name="[[label]]">
-			<div>
-				<a href="[[url]]" target="_blank">[[id]]</a>
-			</div>
+      <aspen-field-wrapper label="[[label]]" icon="[[icon]]" name="[[label]]">
+        <div>
+          <a href="[[url]]" target="_blank">[[id]]</a>
+        </div>
+      </aspen-field-wrapper>
+    `;
+  }
 
-		</aspen-field-wrapper>`;
-    }
+  /**
+   * String providing the tag name to register the element under.
+   */
+  static get is() {
+    return "aspen-link-field";
+  }
 
-    /**
-     * String providing the tag name to register the element under.
-     */
-    static get is() {
-        return 'aspen-link-field';
-    }
+  /**
+   * Instance of the element is created/upgraded. Use: initializing state,
+   * set up event listeners, create shadow dom.
+   * @constructor
+   */
+  constructor() {
+    super();
+  }
 
-    /**
-     * Instance of the element is created/upgraded. Use: initializing state,
-     * set up event listeners, create shadow dom.
-     * @constructor
-     */
-    constructor() {
-        super();
-    }
+  /**
+   * Use for one-time configuration of your component after local DOM is initialized.
+   */
+  ready() {
+    super.ready();
 
-    /**
-     * Use for one-time configuration of your component after local DOM is initialized. 
-     */
-    ready() {
-        super.ready();
-
-        afterNextRender(this, function () {
-
-        });
-    }
-
+    afterNextRender(this, function() {});
+  }
 }
 
 window.customElements.define(AspenLinkField.is, AspenLinkField);
